2df2477c0c682e2119dcc0b2073177995aa68ecc
slakh <s.lakhani@hotmail.ca>
2020-10-12 10:16:49 -0400

    updated backup script to remove temp files

--- backup_to_github.sh
+++ backup_to_github.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Script which syncs my work from my VM o github; as VM does not have internet connectivity
 scp -r -P 2222 mininet@127.0.0.1:/home/mininet/cs144_lab3/router ./ || { echo 'scp failed'; exit 1; }
+cd router
+make clean
+cd ..
 git add .
 git status
 echo 'Enter commit message here, or enter to skip:'
