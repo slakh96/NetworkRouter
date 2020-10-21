849b3132ae1a486adb03cce311935f7fca66a790
slakh <s.lakhani@hotmail.ca>
2020-10-20 20:58:55 -0400

    removed the sending arp message for icmp, for testing purposes

--- router/sr_router.c
+++ router/sr_router.c
@@ -631,15 +631,15 @@ uint8_t ether_shost[ETHER_ADDR_LEN], uint8_t ether_dhost[ETHER_ADDR_LEN]) {
 		fprintf(stderr, "No best match found\n");
 		return;
 	}
-
-	/*Check ARP cache for MAC address corresponding to next-hop ip*/
+	/*
+	/*Check ARP cache for MAC address corresponding to next-hop ip
 	struct sr_arpentry *arp_cache_entry = sr_arpcache_lookup(&(sr->cache),
 		best_match->dest.s_addr);
 	
 	if (arp_cache_entry == NULL){
 		/*unsigned char broadcast_mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};*/
 		/*TODO: Might have to make the ethernet packets destination address
-		the broadcast addr*/
+		the broadcast addr
 		
 		struct sr_arpreq *arp_req = sr_arpcache_queuereq(&(sr->cache), 
 			best_match->gw.s_addr, packet, len, best_match->interface);
@@ -650,7 +650,7 @@ uint8_t ether_shost[ETHER_ADDR_LEN], uint8_t ether_dhost[ETHER_ADDR_LEN]) {
 			return;
 		}
 		return;
-	}
+	}*/
 
 	/*uint8_t *new_packet = calloc(1, len);
 
@@ -699,11 +699,11 @@ uint8_t ether_shost[ETHER_ADDR_LEN], uint8_t ether_dhost[ETHER_ADDR_LEN]) {
 
 	print_hdrs(packet, len);*/
 
-	memcpy(ethernet_packet->ether_dhost, arp_cache_entry->mac, 
-		ETHER_ADDR_LEN);
+	/*memcpy(ethernet_packet->ether_dhost, arp_cache_entry->mac, 
+		ETHER_ADDR_LEN);*/
 
 	int status = sr_send_packet(sr, packet, len, best_match->interface);
-	free(arp_cache_entry);
+	/*free(arp_cache_entry);*/
 	if (status != 0) {
 		fprintf(stderr, "Error when sending icmp reply\n");
 		return;
